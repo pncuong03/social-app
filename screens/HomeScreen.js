0545708ea9e8046d41290006daaa2fea291ccc3e
pncuong03 <phamngoccuonggl3022002@gmail.com>
2024-03-08 02:44:28 +0700

    UILogin

---
+++ screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <View className="">
+      <View className="flex pt-12 pl-5">
+        <TouchableOpacity>
+          <Text className="text-2xl font-bold text-blue-500">Pacebook</Text>
+        </TouchableOpacity>
+        <View>
+            <Image source={require("../assets/svg/home.svg")}/>
+            <Image source={require("../assets/svg/invitation.svg")}/>
+            <Image source={require("../assets/svg/person.svg")}/>
+            <Image source={require("../assets/svg/notification.svg")}/>
+            <Image source={require("../assets/svg/menu.svg")}/>
+        </View>
+      </View>
+    </View>
+  );
+}
