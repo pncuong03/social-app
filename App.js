2f74e000778f7f09a834dc894e673a411ceb4fdd
Phạm Ngọc Cường <Cuong@CUOWNGF>
2024-03-12 02:49:20 +0700

    update

--- App.js
+++ App.js
@@ -4,20 +4,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import HomeScreen from './screens/HomeScreen';
+import Navigation from './navigation/Navigation';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'> 
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignUp" component={SignupScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  // return (
+  //   <NavigationContainer>
+  //     <Stack.Navigator initialRouteName='Login'> 
+  //       <Stack.Screen name="Login" component={LoginScreen} />
+  //       <Stack.Screen name="SignUp" component={SignupScreen} />
+  //       <Stack.Screen name="Home" component={HomeScreen} />
+        
+  //     </Stack.Navigator>
+     
+  //   </NavigationContainer>
+  // );
+  return <Navigation />
 }
 
 //screenOptions={{headerShown: false}}
