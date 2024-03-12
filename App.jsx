551a66cdf9fd0a1c375ae8c9762e498a36b3316d
Phạm Ngọc Cường <Cuong@CUOWNGF>
2024-03-12 13:44:06 +0700

    update

---
+++ App.jsx
@@ -0,0 +1,42 @@
+import { StatusBar } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import LoginScreen from './src/screens/LoginScreen';
+import { Colors } from './src/utils/Colors';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import RegisterScreen from './src/screens/RegisterScreen';
+import MainScreen from './src/screens/MainScreen';
+// import auth from '@react-native-firebase/auth';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+  // const [user, setUser] = useState();
+
+  // const onAuthStateChanged = (user) => setUser(user);
+
+  // useEffect(() => {
+  //   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+  //   return subscriber;
+  // }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor={Colors.white} barStyle='dark-content' />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* {user ? */}
+          {/* <Stack.Screen name="MainScreen" component={MainScreen} /> */}
+           {/* : */}
+          <>
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+            <Stack.Screen name="MainScreen" component={MainScreen} />
+          </>
+        {/* } */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
---
+++ src/components/PostFooter.jsx
@@ -0,0 +1,114 @@
+import {View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import Like from '../assets/images/like.jpeg';
+import Shock from '../assets/images/shock.jpeg';
+import Heart from '../assets/images/heart.jpeg';
+import {Colors} from '../utils/Colors';
+import VectorIcon from '../utils/VectorIcon';
+
+const PostFooter = ({data}) => {
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLikePress = () => {
+    setIsLiked(!isLiked);
+  };
+
+  const handleCommentPress = () => {
+    // Xử lý khi nút comment được nhấn
+  };
+
+  const handleSharePress = () => {
+    // Xử lý khi nút share được nhấn
+  };
+
+  return (
+    <View style={styles.postFooterContainer}>
+      <View style={styles.footerReactionSec}>
+        <View style={styles.row}>
+          <TouchableOpacity onPress={handleLikePress}>
+            <Image source={Like} style={styles.reactionIcon} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleCommentPress}>
+            <Image source={Shock} style={styles.reactionIcon} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handleSharePress}>
+            <Image source={Heart} style={styles.reactionIcon} />
+          </TouchableOpacity>
+          <Text style={styles.reactionCount}>{data.reactionCount}</Text>
+        </View>
+        <Text style={styles.reactionCount}>{data.comments} comments</Text>
+      </View>
+      <View style={styles.userActionSec}>
+        <TouchableOpacity onPress={handleLikePress}>
+          <View style={styles.row}>
+            <VectorIcon
+              name={isLiked ? "like1" : "like2"}
+              type="AntDesign"
+              size={25}
+              color={isLiked ? '#384CFF' : Colors.grey}
+            />
+            <Text style={styles.reactionCount}>Like</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleCommentPress}>
+          <View style={styles.row}>
+            <VectorIcon
+              name="chatbox-outline"
+              type="Ionicons"
+              size={25}
+              color={Colors.grey}
+            />
+            <Text style={styles.reactionCount}>Comment</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleSharePress}>
+          <View style={styles.row}>
+            <VectorIcon
+              name="arrow-redo-outline"
+              type="Ionicons"
+              size={25}
+              color={Colors.grey}
+            />
+            <Text style={styles.reactionCount}>Share</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  reactionIcon: {
+    height: 20,
+    width: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  postFotterContainer: {
+    padding: 16,
+  },
+  reactionCount: {
+    color: Colors.grey,
+    fontSize: 14,
+    paddingLeft: 5,
+  },
+  footerReactionSec: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.lightgrey,
+    padding: 15,
+  },
+  userActionSec: {
+    marginTop: 15,
+    marginBottom: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+});
+
+export default PostFooter;
