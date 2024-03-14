32d6d84df3644dbce6415a2b425789dd10e3d903
Nampti <nguyenhoainama3k55@gmail.com>
2024-03-14 09:50:10 +0700

    test 2

--- src/screens/NewPost.jsx
+++ src/screens/NewPost.jsx
@@ -93,7 +93,7 @@ export default function NewPost() {
                             marginBottom: 5,
                             fontSize: 16,
                         }}>
-                            Nguyễn Thị B</Text>
+                            Nguyễn Thị C</Text>
                         <Menu onSelect={handlePrivacyOption} >
                             <MenuTrigger>
                                 <View style={{
